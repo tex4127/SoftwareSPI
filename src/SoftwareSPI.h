52345b6fc6c89838639d242840aec04703d998bb
tex4127 <95382726+tex4127@users.noreply.github.com>
2025-09-06 10:30:53 -0400

    Update SoftwareSPI.h

--- src/SoftwareSPI.h
+++ src/SoftwareSPI.h
@@ -97,7 +97,37 @@ class SSPIClass{ //: public SPIClass{
                     (word&0x4000) >> 13 |
                     (word&0x8000) >> 15;
             }
-            /* to do, finish this method | above flipping is valid */
+            uint16_t bv = 0;
+            uint8_t d = fDelay >> 1;
+            uint8_t cpst = cpol ? 1 : 0;
+            for (uint8_t bsh = 0; bsh < 16; bsh++){
+                if(cpha){
+                    cpst ^= 1;
+                    digitalWrite(sck, cpst);
+                    wait(d); 
+                }
+                //Write current bit to mosi
+                digitalWrite(mosi, (word & (1 << bsh)) ?  HIGH : LOW);
+                wait(d);
+                //Shift clock
+                cpst ^= 1;
+                digitalWrite(sck, cpst);
+                bv = digitalRead(miso);
+                //Store the bit in o based on data order
+                if(dataOrder == MSBFIRST){
+                    o <<= 1;
+                    o |= bv;
+                }else{
+                    o >>= 1;
+                    o |= bv << 15;
+                }
+                wait(d);
+                if(!cpha){
+                    cpst ^= 1;
+                    digitalWrite(sck, cpst);
+                }
+            }
+            return 0;
         }
         /// @brief Transfers a buffer of data to our SPI device and stores the response into the buffer
         /// @param buffer buffer of data to be written to to the SPI Device
